de7e8903b4896a9c5039729caebb4920e534ef03
Alexandra Sumina <oleksandra.sumina@vanarts.com>
2023-06-23 13:39:26 -0700

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule, Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+
+  {path: '/about', component: AboutComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
