7417d39732f3d9f657e8985b74e845db5e26cd06
Alexandra Sumina <oleksandra.sumina@vanarts.com>
2023-08-11 10:56:07 -0700

    class bind home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -27,6 +27,7 @@ export class HomeComponent implements OnInit{
   homeNav = 'show';
   mainNav = 'hide';
   loading = true;
+  hideMap = "display: none"
 
   // timeout(){
   //   setTimeout(() => {})
@@ -38,6 +39,7 @@ export class HomeComponent implements OnInit{
   ngOnInit(): void {
     this.http.get<NavPage>(this.serverURL + '/api/page-nav?populate=*').subscribe(res => {
       this.loading = false;
+      this.hideMap = ''
       console.log(res)
       this.navPage = res
       this.phoneNum = res.data.attributes.PhoneNumber;
